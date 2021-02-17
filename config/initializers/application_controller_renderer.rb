97ff7769e24b9133496618b58c814ab518badf9c
taknov26 <tak.nov26@gmail.com>
2021-02-17 12:29:52 +0000

    [Finish]5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'affdbdfcef6919edec48eeef708f223573a3c346a1b5751dd55de6de8358e1e05ba09a3a1cf748008933a9860f5c646a310cc391f32abc53e8bf0942816c67a9'
